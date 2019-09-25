883b0ba445a91e11a858888b514fdd77c013a2fd
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-09-25 17:15:37 +0100

    Responsive navigation bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,41 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => (
   <header>
-    <nav>
-      <ul>
-        <li><Link to='/'>About</Link></li>
-        <li><Link to='/builds'>Builds</Link></li>
-        <li><Link to='/contact'>Contact</Link></li>
+    <nav className="fixed z-10 bottom-0 w-100 bg-black flex flex-row left-0-l w-auto-l h-100-l">
+      <ul className="list w-100 flex justify-around flex-column-l justify-center-l">
+        <li>
+          <NavLink
+            exact
+            to="/"
+            className="db white link tc pv2 ph3 hover-bg-green pv3-ns"
+          >
+            <div className="icon about center mb2"></div>
+            <span className="ttu f6 b-ns f5-ns">About</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/builds"
+            className="db white link tc pv2 ph3 hover-bg-green pv3-ns"
+          >
+            <div className="icon builds center mb2"></div>
+            <span className="ttu f6 b-ns f5-ns">Builds</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/contact"
+            className="db white link tc pv2 ph3 hover-bg-green pv3-ns"
+          >
+            <div className="icon contact center mb2"></div>
+            <span className="ttu f6 b-ns f5-ns">Contact</span>
+          </NavLink>
+        </li>
       </ul>
     </nav>
   </header>
-)
+);
 
 export default Header
