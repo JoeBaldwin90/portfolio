52b43e5b37f870e15377bf3249bcba3c5141190c
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-09-27 18:37:50 +0100

    Cleaner line-height and paragraph spacing

--- src/components/About.js
+++ src/components/About.js
@@ -9,22 +9,21 @@ const About = () => (
         <h2 className="f4 f3-ns fw2 fw3-ns ttu blue mb3">
           Junior Web Developer
         </h2>
-        <p className="info-copy f5 f4-l fw5 green tracked">
-          Joe and I worked together at Loco2 for almost four years. During that
-          time, we built a solid working relationship - often remotely via Slack
-          and video chat.{" "}
+        <p className="info-copy f5 f4-l fw5 white">
+          Driven by my desire to build and create, I decided to skill-up and
+          begin a new career path in January 2019. Since then I've been enjoying
+          the intellectual challenge of teaching myself to build full-stack web
+          applications through various online courses
         </p>
-        <p className="info-copy f5 f4-l fw5 green tracked">
-          Joe is incredibly hard-working - bright, thoughtful and conscientious.
-          I was always impressed by his appetite for new information, projects
-          and responsibility, so his move into the role of team leader at
-          Loco2's support team was well deserved.{" "}
+        <p className="info-copy f5 f4-l fw5 white">
+          At the moment I'm thoroughly enjoying working with ReactJS and Redux.
+          Javascript is my preferred language of choice, but I'm familiar and
+          happy to work with Ruby too.
         </p>
-        <p className="info-copy f5 f4-l fw5 green tracked">
-          He is ambitious and unusually adept at self-reflection which gives him
-          an edge when he puts himself forward for new opportunities. I have no
-          doubt that he will continue to thrive at Loco2 or elsewhere, and I
-          will be cheering on his success.
+        <p className="info-copy f5 f4-l fw5 white">
+          I’m currently looking for a junior frontend web development
+          opportunity. I'd like to work on creative and challenging projects
+          with a mindful, communicative and supportive team.
         </p>
       </div>
     </div>
