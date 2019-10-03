4bea3eaddafe86d88306a95295cd032750fdbada
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-10-03 16:32:58 +0100

    Improved main content display for smaller screens

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,7 @@ import Builds from './Builds'
 import Contact from "./Contact";
 
 const Main = () => (
-  <main className="nav-margin w-100 min-vh-100-ns flex flex-start items-center justify-center pa3 pa4-ns ph5-l">
+  <main className="nav-margin w-100 min-vh-100-ns flex flex-start items-center-l justify-center pa3 pa4-ns ph5-l">
     <Switch>
       <Route exact path="/" component={About} />
       <Route path="/builds" component={Builds} />
