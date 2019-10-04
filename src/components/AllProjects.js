c32862a5a2d990792261445ba80aa70358a05196
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-10-04 13:39:24 +0100

    Refactor All Projects page

--- src/components/AllProjects.js
+++ src/components/AllProjects.js
@@ -3,11 +3,42 @@ import ProjectsApi from "../data/projects-api.js";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Overlay = ({ overlay }) => (
-  <span className={`f3 f2-ns pink ${overlay ? "visible" : "hidden"}`}>
+const Screenshot = ({url}) => {
+  return (
+    <div className="min-image-size mb2">
+      <img src={url} className="br3" alt="Project screenshot" />
+    </div>
+  );
+}
+
+const Overlay = ({ hover }) => (
+  <span className={`f3 f2-ns pink ${hover ? "visible" : "hidden"}`}>
     <FontAwesomeIcon icon="eye" size="m" />
   </span>
 );
+
+const Title = ({ name, type, overlay }) => {
+  return (
+    <div>
+      <div className="flex items-center justify-between">
+        <h1 className="pink mr2">{name}</h1>
+        <Overlay hover={overlay} />
+      </div>
+
+      <h2 className="mb2 f3 blue">{type}</h2>
+    </div>
+  );
+};
+
+const Technologies = ({technologies}) => {
+  return (
+    <div>
+      {technologies.map(t => (
+        <span className="mr2 mb1 f6 dib tag">{t}</span>
+      ))}
+    </div>
+  );
+}
 
 // Each project component links to the project show page
 class Project extends React.Component {
@@ -34,7 +65,7 @@ class Project extends React.Component {
   }
 
   render() {
-    const { project, url, name, type, technologies } = this.props;
+    const { project } = this.props;
     return (
       <Link to={`/builds/${project}`} className="link mb3 w-100 w-40-l">
         <div
@@ -43,22 +74,9 @@ class Project extends React.Component {
           onMouseEnter={this.toggleOverlay}
           onMouseLeave={this.closeOverlay}
         >
-          <div className="min-image-size mb2">
-            <img src={url} className="br3" alt="Project screenshot" />
-          </div>
-
-          <div className="flex items-center justify-between">
-            <h1 className="pink mr2">{name}</h1>
-            <Overlay {...this.state} {...this.props} />
-          </div>
-
-          <h2 className="mb2 f3 blue">{type}</h2>
-
-          <div>
-            {technologies.map(t => (
-              <span className="mr2 mb1 f6 dib tag">{t}</span>
-            ))}
-          </div>
+          <Screenshot {...this.props} />
+          <Title {...this.state} {...this.props} />
+          <Technologies {...this.props} />
         </div>
       </Link>
     );
