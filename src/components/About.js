091c29b078884f3ef49b7ed7ba12432880648a85
Joe Baldwin <46322541+JoeBaldwin90@users.noreply.github.com>
2020-05-11 18:38:43 +0100

    2020 refix (#3)
    * Updated About text
    
    * Product detail page style tweak
    
    * Updated product data

--- src/components/About.js
+++ src/components/About.js
@@ -1,71 +1,70 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import LinkButton from './LinkButton';
-import Fade from 'react-reveal/Fade';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import LinkButton from "./LinkButton";
+import Fade from "react-reveal/Fade";
 
 const Icons = () => (
-  <div className="logos dn db-ns w-100 flex-ns flex-wrap-ns mb3">
+  <div className='logos dn db-ns w-100 flex-ns flex-wrap-ns mb3'>
     <Fade cascade>
-      <FontAwesomeIcon icon={['fab', 'html5']} className="icon" />
-      <FontAwesomeIcon icon={['fab', 'css3-alt']} className="icon" />
-      <FontAwesomeIcon icon={['fab', 'sass']} className="icon" />
-      <FontAwesomeIcon icon={['fab', 'js']} className="icon" />
-      <FontAwesomeIcon icon={['fab', 'react']} className="icon" />
-      <FontAwesomeIcon icon={['fab', 'node-js']} className="icon" />
-      <FontAwesomeIcon icon={['fab', 'aws']} className="icon" />
-      <FontAwesomeIcon icon="database" className="icon" />
-      <FontAwesomeIcon icon={['fab', 'trello']} className="icon" />
-      <FontAwesomeIcon icon={['fab', 'slack']} className="icon" />
-      <FontAwesomeIcon icon={['fab', 'github']} className="icon" />
+      <FontAwesomeIcon icon={["fab", "html5"]} className='icon' />
+      <FontAwesomeIcon icon={["fab", "css3-alt"]} className='icon' />
+      <FontAwesomeIcon icon={["fab", "sass"]} className='icon' />
+      <FontAwesomeIcon icon={["fab", "js"]} className='icon' />
+      <FontAwesomeIcon icon={["fab", "react"]} className='icon' />
+      <FontAwesomeIcon icon={["fab", "node-js"]} className='icon' />
+      <FontAwesomeIcon icon={["fab", "aws"]} className='icon' />
+      <FontAwesomeIcon icon='database' className='icon' />
+      <FontAwesomeIcon icon={["fab", "trello"]} className='icon' />
+      <FontAwesomeIcon icon={["fab", "slack"]} className='icon' />
+      <FontAwesomeIcon icon={["fab", "github"]} className='icon' />
     </Fade>
   </div>
 );
 
 const About = () => (
-  <div className="w-100 flex flex-start items-center-ns">
-    <div className="about mw6 mw8-l center db flex-l">
-      <div className="profile br3 w-100 w-60-ns w-35-l">{/* Image */}</div>
-      <div className="info w-100 pl3-l w-65-l">
-        <h1 className="f2 f1-ns b pink ttu">Joe Baldwin</h1>
-        <h2 className="f4 f3-ns fw2 fw3-ns ttu blue mb2">
+  <div className='w-100 flex flex-start items-center-ns'>
+    <div className='about mw6 mw8-l center db flex-l'>
+      <div className='profile br3 w-100 w-60-ns w-35-l'>{/* Image */}</div>
+      <div className='info w-100 pl3-l w-65-l'>
+        <h1 className='f2 f1-ns b pink ttu'>Joe Baldwin</h1>
+        <h2 className='f4 f3-ns fw2 fw3-ns ttu blue mb2'>
           Junior Web Developer
         </h2>
 
         <Icons />
 
-        <p className="info-copy f5 f4-l fw5 white">
+        <p className='info-copy f5 f4-l fw5 white'>
           I enjoy the intrinsic reward and process of learning. Be it Spanish,
-          how to fruit gourmet mushrooms or{' '}
+          how to fruit gourmet mushrooms or{" "}
           <a
-            href="https://youtu.be/9bO77IrvYSo?t=20"
-            className="link dim"
-            target="_blank"
-            rel="noopener noreferrer"
+            href='https://youtu.be/9bO77IrvYSo?t=20'
+            className='link dim'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             how to go faster than 60mph on a skateboard
           </a>
-          . Now, after being inspired by developers at the recently-acquired
-          tech startup, Loco2, I’m teaching myself to code.
+          .
         </p>
-        <p className="info-copy f5 f4-l fw5 white">
-          At Loco2 I’ve developed an enthusiastic and pragmatic approach to
-          problem solving and a passion for working in agile and collaborative
-          environments. Since my promotion to Team Lead I’ve come to understand
-          the importance of open and honest communication and how to take
-          ownership of, and manage, complex projects over time.
+        <p className='info-copy f5 f4-l fw5 white'>
+          I have professional experience working with Angular, but I build MERN
+          applications in my spare time. When I'm not programming, I'm usually
+          rock climbing, practicing yoga or out in nature seeking adventure.
         </p>
-        <p className="info-copy f5 f4-l fw5 white">
-          I’m currently looking for a junior frontend web development
-          opportunity where I can advance my understanding of Javascript, React
-          and Redux.
+        <p className='info-copy f5 f4-l fw5 white'>
+          Due to COVID redundancies, I’m actively seeking a new React.js web
+          development role. My personality suits a collaborative, innovative
+          and pragmatic team. If that's you, then <a href="https://www.linkedin.com/in/jbaldwin90" className='link dim'
+            target='_blank'
+            rel='noopener noreferrer'>let's get connected</a>!
         </p>
 
-        <div className="tl pt3">
+        <div className='tl pt3'>
           <LinkButton
-            href="https://portfolio-images-jb.s3.eu-west-2.amazonaws.com/JoeBaldwin_CV.pdf"
-            logo="eye"
-            title="Open CV"
-            css="pa2 pa3-ns f6 f5-ns w-50 center ml0-ns mr0-ns"
+            href='https://portfolio-images-jb.s3.eu-west-2.amazonaws.com/Joe_Baldwin_CV.pdf'
+            logo='eye'
+            title='Open CV'
+            css='pa2 pa3-ns f6 f5-ns w-50 center ml0-ns mr0-ns'
           />
         </div>
       </div>
--- src/components/ProjectShow.js
+++ src/components/ProjectShow.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import ProjectsApi from '../data/projects-api.js';
-import { Link } from 'react-router-dom';
-import LinkButton from './LinkButton';
+import React from "react";
+import ProjectsApi from "../data/projects-api.js";
+import { Link } from "react-router-dom";
+import LinkButton from "./LinkButton";
 
 const BackButton = () => (
-  <Link to="/builds" className="dib mb3">
-    <h2 className="link ttu tag f5 f4-ns center dib pink">&lt; Back</h2>
+  <Link to='/builds' className='dib mb3'>
+    <h2 className='link ttu tag f5 f4-ns center dib pink'>&lt; Back</h2>
   </Link>
 );
 
 const ProjectInfo = ({ name, description }) => (
-  <div className="mb2">
-    <h1 className="blue ttu pt3">{name}</h1>
-    <p className="f5 f4-ns lh-copy lh-title-ns">{description}</p>
+  <div className='mb2'>
+    <h1 className='blue ttu pt3'>{name}</h1>
+    <p className='f5 f4-ns lh-copy lh-title-ns'>{description}</p>
   </div>
 );
 
 const Technologies = ({ technologies }) => (
   <div>
-    <h2 className="green pt2 ttu">Technologies used: </h2>
-    <ul className="flex justify-between flex-wrap">
-      {technologies.map(tech => (
-        <li key={tech} className="mv2 mr2 mr4-ns pink f6">
+    <h2 className='green pt2 ttu'>Technologies used: </h2>
+    <ul className='flex justify-between flex-wrap'>
+      {technologies.map((tech) => (
+        <li key={tech} className='mv2 mr2 mr4-ns pink f6'>
           <code>{tech}</code>
         </li>
       ))}
@@ -30,22 +30,24 @@ const Technologies = ({ technologies }) => (
 );
 
 const Links = ({ website, github }) => (
-  <ul className="mv3 flex flex-column items-center flex-row-ns justify-between-ns">
-    <li className="w-70 w-40-ns mb3">
-      <LinkButton
-        href={website}
-        logo="eye"
-        title="View project"
-        css="pa2 pa3-ns f6 f5-ns"
-      />
+  <ul className='mv3 flex flex-column items-center flex-row-ns justify-between-ns'>
+    <li className='w-70 w-40-ns mb3'>
+      {website && (
+        <LinkButton
+          href={website}
+          logo='eye'
+          title='View project'
+          css='pa2 pa3-ns f6 f5-ns'
+        />
+      )}
     </li>
-    <li className="w-70 w-40-ns mb3">
+    <li className='w-70 w-40-ns mb3'>
       {github && (
         <LinkButton
           href={github}
-          logo="github"
-          title="See the code"
-          css="pa2 pa3-ns f6 f5-ns"
+          logo='github'
+          title='See the code'
+          css='pa2 pa3-ns f6 f5-ns'
         />
       )}
     </li>
@@ -55,22 +57,22 @@ const Links = ({ website, github }) => (
 // Project looks up the project using the number parsed from
 // the URL's pathname. If no project is found with the given
 // number, then a "project not found" message is displayed.
-const ProjectShow = props => {
+const ProjectShow = (props) => {
   const project = ProjectsApi.get(parseInt(props.match.params.number, 10));
 
   if (!project) {
     return (
       <div>
-        <div className="mb3">Sorry, that project doesn't exist</div>
+        <div className='mb3'>Sorry, that project doesn't exist</div>
         <BackButton />
       </div>
     );
   }
 
   return (
-    <div className="w-100 mw6-m mw7-ns">
+    <div className='w-100 mw6-m mw7-ns'>
       <BackButton />
-      <img src={project.url} className="br3" alt="Project screenshot" />
+      <img src={project.url} className='br3 w-100' alt='Project screenshot' />
       <ProjectInfo {...project} />
       <Technologies {...project} />
       <Links {...project} />
